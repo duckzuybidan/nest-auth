2632a11ba90205b6fcfc185506f0e38c5ff9dd36
duckzuybidan <126694297+duckzuybidan@users.noreply.github.com>
2025-07-05 22:12:31 +0700

    123

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -151,7 +151,7 @@ export class AuthController {
     return this.authService.resendOtp(payload);
   }
 
-  @Post('verify-userssssssss')
+  @Post('verify-users')
   @HttpCode(HttpStatus.OK)
   @Swagger({
     summary: 'Verify user',
